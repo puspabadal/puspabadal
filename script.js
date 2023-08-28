9deac82124ff5272963ff8d46081f5c13b6cc54b
Badalpuspa001 <138523625+puspabadal@users.noreply.github.com>
2023-08-28 17:40:42 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "programmer", "Designer", "Freelancer","Editor"],
+        strings: ["YouTuber", "Developer", "Designer", "Freelancer","Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "programmer", "Designer", "Freelancer","Editor"],
+        strings: ["YouTuber", "Developer", "Designer", "Freelancer","Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
